cd1b6f9da7408e2da58fa90c92637c5957c170b3
MiguelGudalupeGomez <miguelguadalupegomez@outlook.com>
2023-05-05 18:55:27 -0600

    paso 222
    <<<<<<

--- Generacion_variables_aleatorias/src/main/java/com/variablesaleatorias/generacion_variables_aleatorias/Generacion_variables_aleatorias.java
+++ Generacion_variables_aleatorias/src/main/java/com/variablesaleatorias/generacion_variables_aleatorias/Generacion_variables_aleatorias.java
@@ -13,5 +13,6 @@ public class Generacion_variables_aleatorias {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hola GitHub");
+        System.out.println("Hola GitHub");
     }
 }
