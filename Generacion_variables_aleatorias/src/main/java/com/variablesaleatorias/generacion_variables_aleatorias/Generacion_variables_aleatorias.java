82447d027b98a5f7e962242e76e01d4cc0cea082
MiguelGudalupeGomez <117963348+MiguelGudalupeGomez@users.noreply.github.com>
2023-05-05 18:17:38 -0600

    Actualización del respositorio
    Se agregó la carpeta del proyecto.

---
+++ Generacion_variables_aleatorias/src/main/java/com/variablesaleatorias/generacion_variables_aleatorias/Generacion_variables_aleatorias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.variablesaleatorias.generacion_variables_aleatorias;
+
+/**
+ *
+ * @author PC
+ */
+public class Generacion_variables_aleatorias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
