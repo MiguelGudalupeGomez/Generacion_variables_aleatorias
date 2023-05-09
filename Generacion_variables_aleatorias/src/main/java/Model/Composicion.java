da446451063e468c2d1a92e4df5add9716de3f92
Emmanuel <132721030+RojasEmmanuel@users.noreply.github.com>
2023-05-08 23:16:21 -0600

    prueba2
    checando detalles

--- Generacion_variables_aleatorias/src/main/java/Model/Composicion.java
+++ Generacion_variables_aleatorias/src/main/java/Model/Composicion.java
@@ -25,20 +25,20 @@ public class Composicion {
         funcionamientoContinuas();
     }
     
-    public void funcionamientoContinuas(){
+    public void funcionamientoContinuas(){   
         // Generamos 5 números aleatorios con distribución uniforme en el rango [0,1]
         for (int i = 0; i < veces; i++) {
             double u1 = random.nextDouble(); // Generamos un número aleatorio con distribución uniforme en el rango [0,1]
             double x = 2 * u1 - 1; // Aplicamos la transformación para obtener un número aleatorio con distribución uniforme en el rango [-1,1]
-            double u2 = random.nextDouble(); // Generamos otro número aleatorio con distribución uniforme en el rango [0,1]
+            double u2 = random.nextDouble(); // Generamos otro número aleatorio con distribución uniforme en el rango [0,1] 
             while (u2 > (1 + x * x) * Math.exp(-x * x / 2)) { // Comprobamos si el número generado cumple con la distribución deseada
-                u2 = random.nextDouble(); // Si no cumple, generamos otro número aleatorio
+               u2 = random.nextDouble(); // Si no cumple, generamos otro número aleatorio
                 x = 2 * random.nextDouble() - 1; // Aplicamos la transformación de nuevo para obtener otro número aleatorio con distribución uniforme en el rango [-1,1]
             }
             continuas.add(x); // guardamos el número aleatorio generado con la distribución deseada 
             
             double xDiscreta = Math.abs(x*100); 
-            int xDiscretaDos = (int)xDiscreta;
+            int xDiscretaDos = (int)xDiscreta;            
             discretas.add(xDiscretaDos);
         }
     }
