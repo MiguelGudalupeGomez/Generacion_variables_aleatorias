7b73cea71ada62af9b8919a19f7ff67d01b10aa3
Emmanuel <132721030+RojasEmmanuel@users.noreply.github.com>
2023-05-09 08:48:16 -0600

    Nueva actulización
    ...

--- Generacion_variables_aleatorias/src/main/java/View/VistaComposicion.java
+++ Generacion_variables_aleatorias/src/main/java/View/VistaComposicion.java
@@ -10,6 +10,9 @@ package View;
  */
 import javax.swing.*;
 import java.awt.Color; 
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class VistaComposicion extends JFrame{
     
@@ -31,6 +34,46 @@ public class VistaComposicion extends JFrame{
     public void darFormato() {
         
         panel.setBackground(Color.white);
+        
+        titulo.setBounds(280,30,370,40); 
+        titulo.setFont(new Font("Roboto Black",Font.BOLD,26));
+        titulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        
+        pedir.setBounds(300,80,50,30);
+        pedir.setFont(new Font("Roboto",Font.BOLD,14));
+        
+        veces.setBounds(390,80,109,30);
+        Evento e = new Evento(); 
+        veces.addKeyListener(e);
+        
+        generar.setBounds(320,120,89,32);
+        generar.setBackground(new Color(255,255,255));
+        generar.setForeground(new Color(255,204,0));
+        
+        JLabel icono = new JLabel(new ImageIcon("/Dados.png"));
+        icono.setBounds(10,30,200,170);
+        
+        panel.add(icono); 
+        panel.add(generar); 
+        panel.add(veces); 
+        panel.add(pedir); 
+        panel.add(titulo); 
+        
+    }
+    
+    
+    private class Evento implements KeyListener{
+
+        @Override
+        public void keyTyped(KeyEvent e) { // para no ingresar el letras "solo números"
+            if(e.getKeyChar()<'0' || e.getKeyChar()>'9') e.consume(); 
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {}
+
+        @Override
+        public void keyReleased(KeyEvent e) {}
         
     }
 }
