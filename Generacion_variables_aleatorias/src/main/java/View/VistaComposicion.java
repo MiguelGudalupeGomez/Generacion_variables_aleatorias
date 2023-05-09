1a91a3510ccbb8f3a76f8e96bcb2b9dacad8f878
Emmanuel <132721030+RojasEmmanuel@users.noreply.github.com>
2023-05-08 23:31:21 -0600

    Vista Composicion
    Empecé con el desarrollo de la ventana gráfica del método

--- Generacion_variables_aleatorias/src/main/java/View/VistaComposicion.java
+++ Generacion_variables_aleatorias/src/main/java/View/VistaComposicion.java
@@ -9,11 +9,28 @@ package View;
  * @author E m m a n u e l
  */
 import javax.swing.*;
+import java.awt.Color; 
 
 public class VistaComposicion extends JFrame{
     
+    JPanel panel = new JPanel(); 
+    JLabel titulo = new JLabel("MÉTODO DE COMPOSICIÓN");
+    JLabel pedir = new JLabel("Cantidad:");
+    JTextField veces = new JTextField();
+    JButton generar = new JButton("Generar"); 
+    
     public VistaComposicion() {
+        
         setSize(664,445); 
         setLocationRelativeTo(null); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Método de composición");
+        darFormato(); 
+    }
+    
+    public void darFormato() {
+        
+        panel.setBackground(Color.white);
+        
     }
 }
