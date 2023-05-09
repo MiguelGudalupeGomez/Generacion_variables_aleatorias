8b600d62eaca2ec98d4d800d433bb83e4dc1631e
Emmanuel <132721030+RojasEmmanuel@users.noreply.github.com>
2023-05-08 19:02:49 -0600

    Clase Composición
    Cree la clase para el método de composición

---
+++ Generacion_variables_aleatorias/src/main/java/Model/Composicion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author E m m a n u e l
+ */
+public class Composicion {
+    
+}
